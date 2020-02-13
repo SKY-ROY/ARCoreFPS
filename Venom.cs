1ef6691a587f5eebca478b1982bdad9130c9aa94
SKY <akash.rm.roy@gmail.com>
2020-02-13 18:21:06 +0530

    Create Venom.cs

---
+++ Venom.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Venom : MonoBehaviour
+{
+    private Rigidbody rb;
+    private GameObject target;
+    private float moveSpeed;
+    public float rotFrequency = 50f;
+
+    Vector3 directionToTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveSpeed = Random.Range(8f, 10f);
+        MoveSpit();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //health trigger
+        if (collision.gameObject.CompareTag("Player"))//tag == "Player")
+        {
+            Debug.Log("health--");
+            PlayerController.Health -= 5;
+            Destroy(gameObject);
+        }
+    }
+
+    void MoveSpit()
+    {
+        target = GameObject.FindGameObjectWithTag("MainCamera");
+        if (target != null)
+        {
+            directionToTarget = (target.transform.position - transform.position).normalized;
+            rb.velocity = new Vector3(directionToTarget.x * moveSpeed, directionToTarget.y * moveSpeed, directionToTarget.z * moveSpeed);
+        }
+        else
+            rb.velocity = Vector3.zero;
+    }
+}
