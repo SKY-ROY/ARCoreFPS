d81685623c168349b9404cbdb668ba3a295bb126
SKY <akash.rm.roy@gmail.com>
2020-03-14 21:12:18 +0530

    Update BossController.cs

--- BossController.cs
+++ BossController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossController : MonoBehaviour
 {
@@ -75,12 +76,15 @@ public class BossController : MonoBehaviour
 
     [SerializeField]
     float moveSpeed = 2f;
-
     int waypointIndex = 7;
+    float maxHealth;
+    
     public Transform[] waypoints;
+    public Slider bossHealth;
 
     void Start()
     {
+        maxHealth = GetComponent<Target>().health;
         for(int childIndex = 0; childIndex < waypointIndex; childIndex++)
         {
             waypoints[childIndex] = GameObject.Find("BossPath").transform.GetChild(childIndex).transform;
@@ -92,6 +96,7 @@ public class BossController : MonoBehaviour
     void Update()
     {
         MoveBoss();
+        HealthUpdate();
     }
 
     void MoveBoss()
@@ -105,6 +110,11 @@ public class BossController : MonoBehaviour
 
         if (waypointIndex == waypoints.Length)
             waypointIndex = 0;
-    
     }    
+    void HealthUpdate()
+    {
+        float health = GetComponent<Target>().health;
+        bossHealth.value = health / maxHealth;
+        Debug.Log(health + "/" + maxHealth);
+    }
 }
